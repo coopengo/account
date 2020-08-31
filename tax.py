3b8e1d22f6d08dc0e63165d0800b5091006b074b
Nidhal <nidhal.khalgui@coopengo.com>
2020-08-31 04:32:28 +0200

    account: fix acces to tax date property when object is not stored yet Fix #00000

--- tax.py
+++ tax.py
@@ -1138,7 +1138,8 @@ class TaxableMixin(object):
                 for params in self.taxable_lines]
             for line in taxable_lines:
                 l_taxes = Tax.compute(Tax.browse(line.taxes), line.unit_price,
-                    line.quantity, self.tax_date)
+                    line.quantity, getattr(self, 'tax_date',
+                        pool.get('ir.date').today()))
                 for tax in l_taxes:
                     taxline = self._compute_tax_line(**tax)
                     # Base must always be rounded per line as there will be one
